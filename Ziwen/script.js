a30b70a9e78870c5f11228cf0dc980535aedd7aa
siyuansdsd <113959070+siyuansdsd@users.noreply.github.com>
2023-03-13 20:26:56 +1000

    Update script.js

--- Ziwen/script.js
+++ Ziwen/script.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
         // $(".show").hide();
         // $(".hidden").show();
     });
+
+    $("#magic").click(function() {
+        $(this).hide();
+        $(".show").hide();
+        $(".main_title").css("background-color", "rgba(255, 255, 255, 0)");
+        $("*").css("background-color", "#9932cc").ready(function() {
+            // $("*").css({ "animation": "animateColor 4s linear infinite" });
+            setTimeout(() => $("*").css({ "animation": "animateColor 4s linear infinite" }), "10000");
+        });
+        // $(".hidden").show();
+    });
 })
