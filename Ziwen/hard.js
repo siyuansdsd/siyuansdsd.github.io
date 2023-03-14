28260c49af7ce7250c1cf46383db66434701698a
siyuansdsd <113959070+siyuansdsd@users.noreply.github.com>
2023-03-15 00:12:32 +1000

    Create hard.js

---
+++ Ziwen/hard.js
@@ -0,0 +1,26 @@
+var countDownDate = new Date("Aug 15, 2023 08:00:00").getTime();
+
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("myCounter").innerHTML = days + " Days " + hours + " Hours " +
+        minutes + " Minutes " + seconds + " Seconds ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("myCounter").innerHTML = "Celebrate!!!!";
+    }
+}, 1000);
